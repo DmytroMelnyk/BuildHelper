709f58bef548e32d5aebd4ee93da2cbef6b9a9b6
DmytroMelnyk <damelnik87@gamil.com>
2016-03-13 23:41:29 +0200

    Changes: 1. Moved to Nuget package. 2. Emit class was used to instantanate dynamic GetCurrent method.

--- BuildHelper/Helpers/GettingEventArgsExtension.cs
+++ BuildHelper/Helpers/GettingEventArgsExtension.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Linq.Expressions;
 using System.Reflection;
+using System.Reflection.Emit;
 
 namespace BuildHelper
 {
@@ -11,13 +12,20 @@ namespace BuildHelper
 
         static GettingEventArgsExtension()
         {
-            PropertyInfo _CurrentPropertyInfo = typeof(GettingEventArgs).
-                GetProperty("Current", BindingFlags.NonPublic | BindingFlags.Instance);
-            ParameterExpression _Parameter = Expression.Parameter(typeof(GettingEventArgs));
-            MethodCallExpression _MethodCall = Expression.Call(_Parameter, _CurrentPropertyInfo.GetGetMethod(true));
-            _GetDelegate = Expression.
-                    Lambda<Func<GettingEventArgs, int>>(_MethodCall, _Parameter).
-                    Compile();
+            //PropertyInfo _CurrentPropertyInfo = typeof(GettingEventArgs).
+            //    GetProperty("Current", BindingFlags.NonPublic | BindingFlags.Instance);
+            //ParameterExpression _Parameter = Expression.Parameter(typeof(GettingEventArgs));
+            //MethodCallExpression _MethodCall = Expression.Call(_Parameter, _CurrentPropertyInfo.GetGetMethod(true));
+            //_GetDelegate = Expression.
+            //        Lambda<Func<GettingEventArgs, int>>(_MethodCall, _Parameter).
+            //        Compile();
+
+            var getDelegateMethod = new DynamicMethod("getDelegateMethod", typeof(int), new[] { typeof(GettingEventArgs) }, typeof(GettingEventArgs));
+            var ilGen = getDelegateMethod.GetILGenerator();
+            ilGen.Emit(OpCodes.Ldarg_0);
+            ilGen.Emit(OpCodes.Ldfld, typeof(GettingEventArgs).GetField("m_currentNumOperations", BindingFlags.NonPublic | BindingFlags.Instance));
+            ilGen.Emit(OpCodes.Ret);
+            _GetDelegate = (Func<GettingEventArgs, int>)getDelegateMethod.CreateDelegate(typeof(Func<GettingEventArgs, int>));
         }
 
         static public int GetCurrent(this GettingEventArgs arg)
